07005da8fd77e76a58d1aa3d0983ba819fb0eef6
Jelmer Keuzenkamp <jelmer.keuzenkamp@newcraftgroup.com>
2021-05-05 20:01:51 +0200

    extend search

--- podcast_search/__init__.py
+++ podcast_search/__init__.py
@@ -1,8 +1,20 @@
-"""Let's find some podcasts!"""
+# ...
+SEARCH_URL = "https://itunes.apple.com/search"
 
-__version__ = "0.1.0"
+@dataclass
+class Podcast:
+    """Podcast metadata."""
+    id: str
+    name: str
+    author: str
+    url: str
+    feed: Optional[str] = None
+    category: Optional[str] = None
+    image: Optional[str] = None
 
 
-def search(name, count=5):
+def search(name: str, limit: int = 5) -> List[Podcast]:
     """Search podcast by name."""
-    raise NotImplementedError()
+    params = {"term": name, "limit": limit, "media": "podcast"}
+    response = _get(url=SEARCH_URL, params=params)
+    return _parse(response)
